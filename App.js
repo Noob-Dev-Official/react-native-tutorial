0d91366b4d078f869b1f6024b51e1e213f3c8db9
noob_dev54 <muntasirenamalhuda@gmail.com>
2022-11-22 21:50:33 +0000

    init commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	TouchableWithoutFeedback,
+	SafeAreaView,
+	Image,
+	Alert,
+	Button,
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text numberOfLines={1}>
+				Open up App.js to start working on yourasdasasdasasd
+			</Text>
+			<TouchableWithoutFeedback onPress={() => console.log('hello')}>
+				<Image source={require('./assets/icon.png')} style={styles.image} />
+			</TouchableWithoutFeedback>
+			<Button
+				title='Hello'
+				onPress={() =>
+					Alert.alert('Title', 'Message', [
+						{ text: 'Yes', onPress: () => console.log('yes') },
+						{ text: 'No', onPress: () => console.log('no') },
+					])
+				}
+			/>
+			<StatusBar style='auto' />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		margin: 50,
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	image: {
+		width: 100,
+		height: 100,
+		marginTop: 30,
+	},
 });
---
+++ README.md
@@ -0,0 +1,9 @@
+# React Native
+
+## Start app in dev env
+
+`npx expo start --tunnel`
+
+## Publish app to expo
+
+`expo publish`
